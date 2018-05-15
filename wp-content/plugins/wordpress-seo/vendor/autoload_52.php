285152eadd85c72f4fc419c6705c1b3c7d467ff8
Admin Do Not Use Ryan Evaul <ryan@ryanevaul.com>
2018-05-15 18:28:59 +0000

    updated plugin `Yoast SEO` version 7.5

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit14e7595e1172cee8afbf186e7e17e6c0::getLoader();
+return ComposerAutoloaderInitaf12aba68720b15cc8468fcc9f6a7f92::getLoader();
