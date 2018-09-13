a98f4cbf5357e11b8e20b13d906314b305169b32
Admin Do Not Use Nick Klotz <nick.klotz9@gmail.com>
2018-09-13 18:17:16 +0000

    modified theme `Editorial Pro` version 1.1.3

--- wp-content/themes/editorial-pro/layouts/posts/layout-three.php
+++ wp-content/themes/editorial-pro/layouts/posts/layout-three.php
@@ -80,14 +80,17 @@ $post_id = get_the_ID();
 						<?php 
 							if( has_post_thumbnail() ) {
 								the_post_thumbnail( 'editorial-single-large' );
-								
-								
 						
 							} else {
 								$image_src = editorial_pro_image_fallback( 'editorial-single-large' );
 								$image_path = '<img src="'. $image_src[0] .'"/>';
 							}
 						?>
+						<?php if (get_post(get_post_thumbnail_id())->post_excerpt) { // search for if the image has caption added on it ?>
+    <span class="featured-image-caption">
+        <?php echo wp_kses_post(get_post(get_post_thumbnail_id())->post_excerpt); // displays the image caption ?>
+    </span>
+<?php } ?>
 					</figure>
 				</div><!-- .single-post-image -->
 		<?php } ?>
