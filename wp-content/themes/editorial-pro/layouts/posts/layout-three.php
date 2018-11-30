4ab52561b48cdd51a94a09dd4fc64a7b2833c8f0
Admin Do Not Use Zach Meisel <zachmeisel@ymail.com>
2018-11-30 18:15:16 +0000

    modified theme `Editorial Pro` version 1.1.3

--- wp-content/themes/editorial-pro/layouts/posts/layout-three.php
+++ wp-content/themes/editorial-pro/layouts/posts/layout-three.php
@@ -20,6 +20,16 @@ $post_id = get_the_ID();
 	<div class="entry-meta">
 		<?php 
 			editorial_pro_posted_on();
+			<?php
+			if ( function_exists( 'coauthors_posts_links' ) )
+			{
+				coauthors_posts_links();
+			}
+			else
+			{
+				the_author_post_link();
+			}
+		?>
 			editorial_post_views_count();
 			editorial_pro_post_comment();
 		?>
