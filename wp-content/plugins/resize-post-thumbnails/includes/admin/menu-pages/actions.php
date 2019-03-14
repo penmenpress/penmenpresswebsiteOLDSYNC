c68fe66ec2a40a548a00fdb9b89fd77ce3e97d3a
Admin Do Not Use Ryan Evaul <ryan@ryanevaul.com>
2019-03-14 19:11:27 +0000

    deleted plugin `Resize Post Thumbnails` version 1.2

--- wp-content/plugins/resize-post-thumbnails/includes/admin/menu-pages/actions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-// Theme options
-add_filter( 'resize_post_thumbnails__add_menu_page_settings', 'resize_post_thumbnails__menu_page' );
