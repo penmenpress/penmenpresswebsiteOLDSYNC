cac03349d62b5ab99f60eb93251d9a8e42840694
Admin Do Not Use Ryan Evaul <ryan@ryanevaul.com>
2019-03-14 19:11:26 +0000

    deleted plugin `Tech Blogging Theme for AMP` version 1.1

--- wp-content/plugins/tech-amp-theme-master/search.php
+++
@@ -1,8 +0,0 @@
-<?php amp_header() ?>
-<div class="amp-archive">
-	<div class="cntr">
-		<?php amp_loop_template(); ?>
-	</div><!-- /.container -->
-</div><!-- /.amp-archive -->
-    
-<?php amp_footer(); ?>
