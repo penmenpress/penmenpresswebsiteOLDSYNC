2aa286f6bb3eacba3c45e97ebf9d6af7963a522e
Admin Do Not Use Ryan Evaul <ryan@ryanevaul.com>
2020-12-29 01:43:20 +0000

    modified file `wflogs` (after activation of `Wordfence Security` version 7.4.14)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1609810997;s:7:"authKey";s:64:"YQPzzb:5aq.vzkJ#f#J{83c(.r0k4FLJOWG?Iyw<JP%ilUEsu4s# LT]MwZ]iU{$";}
