6255f861424310c3eb7cdd33841f639b3a2528e4
Gitium <gitium@presslabs.com>
2021-03-25 20:52:23 +0000

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1609810997;s:7:"authKey";s:64:"YQPzzb:5aq.vzkJ#f#J{83c(.r0k4FLJOWG?Iyw<JP%ilUEsu4s# LT]MwZ]iU{$";}
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1609810997;s:7:"authKey";s:64:"YQPzzb:5aq.vzkJ#f#J{83c(.r0k4FLJOWG?Iyw<JP%ilUEsu4s# LT]MwZ]iU{$";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;}
