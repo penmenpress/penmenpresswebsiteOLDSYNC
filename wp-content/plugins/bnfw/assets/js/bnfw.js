9f0f892824282fbfe66e0ac09a0ed2c01b04609c
Admin Do Not Use Ryan Evaul <ryan@ryanevaul.com>
2022-02-25 00:16:00 +0000

    updated plugin `Better Notifications for WP` version 1.8.11

--- wp-content/plugins/bnfw/assets/js/bnfw.js
+++ wp-content/plugins/bnfw/assets/js/bnfw.js
@@ -64,6 +64,7 @@ jQuery(document).ready(function($) {
 					return {
 						action: 'bnfw_search_users',
 						query: params.term,
+						bnfw_security: BNFW.bnfw_users_search_ajax_nonce,
 						page: params.page
 					};
 				},
@@ -633,7 +634,7 @@ jQuery(document).ready(function($) {
 
 		if ( body !== '' ) {
 			if ( tinyMCE && tinyMCE.editors && tinyMCE.editors['notification_message'] ) {
-				tinyMCE.editors['notification_message'].selection.setContent( '<div>' + body + '</div>' );
+				tinyMCE.editors['notification_message'].selection.setContent( body );
 			}
 		}
 
